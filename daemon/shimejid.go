0409dd66b00b694810f03ce6c36e243058849f06
York Wang <anyone.1984@gmail.com>
2021-01-15 12:45:29 +0800

    parse configs

--- daemon/shimejid.go
+++ daemon/shimejid.go
@@ -1,7 +1,10 @@
 package daemon
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -9,9 +12,53 @@ import (
 type Shimejid struct {
 }
 
+type Mascot struct {
+	XMLName     xml.Name     `xml:"Mascot"`
+	ActionLists []ActionList `xml:"ActionList"`
+}
+
+type ActionList struct {
+	XMLName xml.Name `xml:"ActionList"`
+	Actions []Action `xml:"Action"`
+}
+
+type Action struct {
+	XMLName          xml.Name          `xml:"Action"`
+	Name             string            `xml:"Name,attr"`
+	Type             string            `xml:"Type,attr"`
+	BorderType       string            `xml:"BorderType,attr"`
+	Class            string            `xml:"Class,attr"`
+	Loop             bool              `xml:"Loop,attr"`
+	Condition        string            `xml:"Condition,attr"`
+	Animations       []Animation       `xml:"Animation"`
+	Actions          []Action          `xml:"Action"`
+	ActionReferences []ActionReference `xml:"ActionReference"`
+}
+
+type ActionReference struct {
+	XMLName xml.Name `xml:"ActionReference"`
+	Name    string   `xml:"Name,attr"`
+}
+
+type Animation struct {
+	XMLName xml.Name `xml:"Animation"`
+	Poses   []Pose   `xml:"Pose"`
+}
+
+type Pose struct {
+	XMLName     xml.Name `xml:"Pose"`
+	Image       string   `xml:"Image,attr"`
+	ImageAnchor string   `xml:"ImageAnchor,attr"`
+	Velocity    string   `xml:"Velocity,attr"`
+	Duration    int      `xml:"Duration,attr"`
+	Sound       string   `xml:"Sound,attr"`
+	Volume      int      `xml:"Volume,attr"`
+}
+
 // Init 启动守护协程
 func Init() *Shimejid {
 	d := new(Shimejid)
+	d.loadActions()
 	go d.run()
 	return d
 }
@@ -25,5 +72,40 @@ func (d *Shimejid) run() {
 	for {
 		fmt.Println("Shimejid running")
 		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func (d *Shimejid) loadActions() {
+	// 加载配置文件
+	file, err := os.Open("conf/actions.xml")
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+	m := Mascot{}
+	err = xml.Unmarshal(data, &m)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	for _, list := range m.ActionLists {
+		for _, action := range list.Actions {
+			fmt.Println(fmt.Sprintf("[Action]            Name: %s, Type; %s, Class; %s, BorderType; %s", action.Name, action.Type, action.Class, action.BorderType))
+			for _, anime := range action.Animations {
+				for _, pose := range anime.Poses {
+					fmt.Println(fmt.Sprintf("- [Pose]            Image: %s ImageAnchor: %s Velocity: %s Duration: %d, Sound: %s, Volume: %d", pose.Image, pose.ImageAnchor, pose.Velocity, pose.Duration, pose.Sound, pose.Volume))
+				}
+			}
+			for _, ref := range action.ActionReferences {
+				fmt.Println(fmt.Sprintf("- [ActionReference] Name: %s", ref.Name))
+			}
+		}
 	}
 }
